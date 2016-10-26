215daa66412da34f3aa87068e9086b2fae1ace69
Tim Mikeladze <tim.mikeladze@gmail.com>
2016-10-25 23:23:43 -0700

    Accounts.ui.config and tests

---
+++ src/client/index.spec.js
@@ -0,0 +1,26 @@
+import chai from 'chai';
+import sinonChai from 'sinon-chai';
+import chaiAsPromised from "chai-as-promised";
+
+import Accounts from './index';
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+
+describe('Accounts', () => {
+  it('throws error on invalid keys', () => {
+    () => Accounts.ui.config({
+      'bad key': 'bad value',
+    }).should.throw.error;
+  });
+  it('redirect hooks must be strings or functions', () => {
+      () => Accounts.ui.config({
+        onSignedInHook: () => null
+      }).should.be.ok;
+      () => Accounts.ui.config({
+        onSignedInHook: ''
+      }).should.be.ok;
+      () => Accounts.ui.config({
+        onSignedInHook: {}
+      }).should.throw.error;
+    });
+});
