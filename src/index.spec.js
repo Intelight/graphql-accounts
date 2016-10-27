6d971a9b606d96ed5fba17fdaf4be8028b517d17
Tim Mikeladze <tim.mikeladze@gmail.com>
2016-10-27 00:33:22 -0700

    Added title to ui config

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,20 +1,26 @@
 import chai from 'chai';
-//eslint-disable-next-line
-import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import chaiAsPromised from "chai-as-promised";
 
 import Accounts from './index';
-//eslint-disable-next-line
-const should = chai.should();
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
 
-describe('graphql-accounts', () => {
-
-  it('sets foo', () => {
-    const accounts = new Accounts();
-    accounts.foo.should.be.true;
+describe('Accounts', () => {
+  it('throws error on invalid keys', () => {
+    () => Accounts.ui.config({
+      'bad key': 'bad value',
+    }).should.throw.error;
   });
-
+  it('redirect hooks must be strings or functions', () => {
+      () => Accounts.ui.config({
+        onSignedInHook: () => null
+      }).should.be.ok;
+      () => Accounts.ui.config({
+        onSignedInHook: ''
+      }).should.be.ok;
+      () => Accounts.ui.config({
+        onSignedInHook: {}
+      }).should.throw.error;
+    });
 });
